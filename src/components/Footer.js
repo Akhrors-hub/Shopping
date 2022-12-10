81b5ad4dbbd3e7e17d85ae3614a4737e8bf92412
Akhror Mirfayizev <mi7akhror@gmail.com>
2022-12-09 22:57:54 -0800

    push the updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,81 @@
 import React from "react"
 import styled from "styled-components"
+import { FaFacebookSquare } from "react-icons/fa";
+import { RiInstagramFill } from "react-icons/ri";
+import { FaTwitterSquare } from "react-icons/fa";
 
 const Container = styled.div`
 display:flex;
 align-items: center;
 justify-content: center;
-background-color: #ff5f34;
+background-color: white;
 `
+const Left = styled.div`
+flex:1;
+padding: 20px;
+
+`
+
+const Center = styled.div`
+flex:1;
+padding: 20px;
+`
+const Right = styled.div`
+flex:1;
+padding: 20px;
+`
+const Description = styled.p`
+margin: 20px 0px;
+`
+const Logo = styled.h1`
+text-align:cener;
+justify-content: center;
+`
+const SocialContainer = styled.div`
+display: flex;
+justify-content: center;
+`
+const SocialIcon = styled.div`
+width: 40px;
+height: 40px;
+border-radius: 50%;
+color: white;
+background-color: #${props=>props.color};
+display: flex;
+align-items: center;
+justify-content: center;
+margin-right: 20px;
+
+`
+ 
 
 const Footer =()=>{
     return (
         <Container>
+            <Left>
+                <Logo> SaleCart.com </Logo>
+                <Description>
+                React has been designed from the start for gradual adoption, and you can use as little or as much React as you need. Perhaps you only want to add some “sprinkles of interactivity” to an existing page. React components are a great way to do that.
 
-            I am a Footer
+The majority of websites aren’t, and don’t need to be, single-page apps. With a few lines of code and no build tooling, try React in a small part of your website. You can then either gradually expand its presence, or keep it contained to a few dynamic widgets.
+                </Description>
+                <SocialContainer>
+                    <SocialIcon color="3B5999">
+                    <FaFacebookSquare />
+                    </SocialIcon >
+                    <SocialIcon color="E4405F">
+                    <RiInstagramFill />
+                    </SocialIcon>
+                    <SocialIcon color="55ACEE">
+                        <FaTwitterSquare />
+                    </SocialIcon>
+                </SocialContainer>
+            </Left>
+<Center>
+
+</Center>
+    <Right>
+        </Right>        
         </Container>
     )
 }
