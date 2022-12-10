33592c4c94c4ade01991da76df81a20b2fc6614b
Akhror Mirfayizev <mi7akhror@gmail.com>
2022-12-09 21:30:33 -0800

    ok

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react"
+import styled from "styled-components"
+
+const Container = styled.div`
+display:flex;
+align-items: center;
+justify-content: center;
+background-color: #ff5f34;
+`
+
+const Footer =()=>{
+    return (
+        <Container>
+
+            I am a Footer
+        </Container>
+    )
+}
+export default Footer;
